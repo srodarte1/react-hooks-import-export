4254cf0c46698f8351de1ccfa741e461057a5546
+ <rodarte_sarah@yahoo.com>
2022-12-19 13:55:51 -0700

    commit tree a9f25f5b536281092a76b335e0e86061a8a81bd8

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,11 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
+export default ColoradoStateParks;
